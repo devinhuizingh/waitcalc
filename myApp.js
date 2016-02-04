eefc43c30f8e160fbbbdbb7d331de9e1b2d9dafc
devinhuizingh <devinhuizingh@gmail.com>
2016-02-04 14:23:05 -0700

    good reponsive working app

--- myApp.js
+++ myApp.js
@@ -1,31 +1,40 @@
 angular.module('myApp', [])
 	.controller('firstController', function($scope) {
-		$scope.mealPrice="0.00"
-		$scope.taxRate=".00"
-		$scope.tipPercent="0.00"
-		$scope.sub="0"
-		$scope.tip="0"
-		$scope.total="0"
-		$scope.tipAdd="0"
-		$scope.mealCount="0"
+		reset()
 
 		$scope.submit = function(){
-			console.log("submit works")
 			$scope.sub=$scope.mealPrice + $scope.mealPrice*$scope.taxRate
 			$scope.tip=$scope.mealPrice*$scope.tipPercent
 			$scope.total=$scope.sub+$scope.tip
+			$scope.mealPrice=null
+			$scope.tipPercent=null
 			
-			$scope.tipAdd=(+$scope.tipAdd)+(+$scope.tip)
-			$scope.mealCount=($scope.mealCount*1)+1
 			
-
-
-
+			$scope.tipAdd += $scope.tip
+			$scope.mealCount++
+			$scope.avg=($scope.tipAdd/$scope.mealCount)
+		
 		}
 
 		$scope.cancel = function(){
-			console.log("cancel works")
+			console.log("click works")
+			$scope.mealPrice=0
+			$scope.taxRate=0
+			$scope.tipPercent=0
 		}
 
+		$scope.reset = reset
+
+		function reset(){
+			$scope.mealPrice=null
+			$scope.taxRate=null
+			$scope.tipPercent=null
+			$scope.sub=0
+			$scope.tip=0
+			$scope.total=0
+			$scope.tipAdd=0
+			$scope.mealCount=0
+			$scope.avg=0
+		}
 
 	})
