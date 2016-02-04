868648b3023af8bb6bd921e0615cf6c29a628f5e
devinhuizingh <devinhuizingh@gmail.com>
2016-02-04 09:55:46 -0700

    customer counter working

--- myApp.js
+++ myApp.js
@@ -3,9 +3,24 @@ angular.module('myApp', [])
 		$scope.mealPrice="0.00"
 		$scope.taxRate=".00"
 		$scope.tipPercent="0.00"
+		$scope.sub="0"
+		$scope.tip="0"
+		$scope.total="0"
+		$scope.tipAdd="0"
+		$scope.mealCount="0"
 
 		$scope.submit = function(){
 			console.log("submit works")
+			$scope.sub=$scope.mealPrice + $scope.mealPrice*$scope.taxRate
+			$scope.tip=$scope.mealPrice*$scope.tipPercent
+			$scope.total=$scope.sub+$scope.tip
+			
+			$scope.tipAdd=($scope.tipAdd)+($scope.tip)
+			$scope.mealCount=($scope.mealCount*1)+1
+			
+
+
+
 		}
 
 		$scope.cancel = function(){
