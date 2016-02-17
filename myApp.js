2bcf7e369f52abfec9d7ab97799c0654ff9687fa
devinhuizingh <devinhuizingh@gmail.com>
2016-02-17 14:24:13 -0700

    added views, animation working, but not forms

--- myApp.js
+++ myApp.js
@@ -1,60 +1,81 @@
-angular.module('myApp', ['ngRoute'])
+angular.module('myApp', ['ngRoute', 'ngAnimate'])
 	.config(['$routeProvider', function($routeProvider){
         $routeProvider.when('/', {
             templateUrl : 'home.html',
-            controller : 'firstController'
+           
         })
         .when('/new-meal', {
             templateUrl : 'new-meal.html',
-            controller : 'firstController'
+            controller : 'newMeal'
         }).when('/my-earnings', {
             templateUrl : 'my-earnings.html',
-            controller : 'firstController'
+            controller : 'myEarnings'
         });
     }])
 
-	.controller('firstController', function($scope, $rootScope) {
-		reset()
+    
+
+	.controller('newMeal', function($scope, $rootScope) {
+		//reset();
+		if ($rootScope.mealCount = "undefined") {$rootScope.mealCount = 0;}
+		$rootScope.tipAdd = 0;
+		$scope.mealCount=$rootScope.mealCount;
 
 		$scope.submit = function(){
-			console.log("there we go")
-			$rootScope.sub=$rootScope.mealPrice + $rootScope.mealPrice*$rootScope.taxRate;
-			$rootScope.tip=$rootScope.mealPrice*$rootScope.tipPercent;
-
-
-			$rootScope.total=$rootScope.sub+$scope.tip;
-			$rootScope.mealPrice=null;
-			$rootScope.tipPercent=null;
 			
+			$scope.sub=$scope.mealPrice + $scope.mealPrice*$scope.taxRate;
+			$scope.tip=$scope.mealPrice*$scope.tipPercent;
+	
+			$scope.total=$scope.sub+$scope.tip;
+			$scope.mealPrice=null;
+			$scope.tipPercent=null;
+						
+			$scope.tipAdd += $scope.tip;
+			$scope.mealCount++;
+			$scope.avg=($scope.tipAdd/$scope.mealCount);
+
+			$rootScope.mealCount = $scope.mealCount;
+			$rootScope.tipAdd =  $scope.tipAdd;
+			$setPristine();
 			
-			$rootScope.tipAdd += $rootScope.tip;
-			$rootSootScope.mealCount++;
-			$rootScope.avg=($rootScope.tipAdd/$rootScope.mealCount);
-			console.log($rootScope.mealCount)
-		
 		}
 
 		$scope.cancel = function(){
-			console.log("click works")
-			$rootScope.mealPrice=0;
-			$rootScope.taxRate=0;
-			$rootScope.tipPercent=0;
+			$scope.mealPrice=0;
+			$scope.taxRate=0;
+			$scope.tipPercent=0;
 		}
 
-		$rootScope.reset = reset;
+		$scope.reset = reset;
 
 		function reset(){
-			$rootScope.mealPrice=null;
-			$rootScope.taxRate=null;
-			$rootScope.tipPercent=null;
-			$rootScope.sub=0;
-			$rootScope.tip=0;
-			$rootScope.total=0;
-			$rootScope.tipAdd=0;
-			$rootScope.mealCount=0;
-			$rootScope.avg=0;
+			$scope.mealPrice=null;
+			$scope.taxRate=null;
+			$scope.tipPercent=null;
+			$scope.sub=0;
+			$scope.tip=0;
+			$scope.total=0;
+			$scope.tipAdd=0;
+			$scope.mealCount=0;
+			$scope.avg=0;
 		}
-
-		
-
 	})
+	.controller('myEarnings', function($scope, $rootScope) {
+		function reset(){
+			$scope.mealPrice=null;
+			$scope.taxRate=null;
+			$scope.tipPercent=null;
+			$scope.sub=0;
+			$scope.tip=0;
+			$scope.total=0;
+			$scope.tipAdd=0;
+			$scope.mealCount=0;
+			$scope.avg=0;
+
+		}
+		console.log($rootScope.mealCount);
+		$scope.mealCount=$rootScope.mealCount;
+	})
+
+
+	
